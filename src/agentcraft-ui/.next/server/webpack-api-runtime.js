0dc50a483d9994f849660658fcdc920e5b3a3940
wyz-honey <wyz_honey@163.com>
2023-09-23 10:56:49 +0800

    feat:test pr

--- src/agentcraft-ui/.next/server/webpack-api-runtime.js
+++ src/agentcraft-ui/.next/server/webpack-api-runtime.js
@@ -204,7 +204,7 @@
 /******/ 		__webpack_require__.f.require = (chunkId, promises) => {
 /******/ 			// "1" is the signal for "already loaded"
 /******/ 			if(!installedChunks[chunkId]) {
-/******/ 				if("pages/api/application/list" == chunkId) {
+/******/ 				if("webpack-api-runtime" != chunkId) {
 /******/ 					installChunk(require("./" + __webpack_require__.u(chunkId)));
 /******/ 				} else installedChunks[chunkId] = 1;
 /******/ 			}
--- src/agentcraft-ui/.next/static/chunks/webpack.js
+++ src/agentcraft-ui/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "18ea0b94110cab59"; }
+/******/ 		__webpack_require__.h = function() { return "58b74908896569d6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
