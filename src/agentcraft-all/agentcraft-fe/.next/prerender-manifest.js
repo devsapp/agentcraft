148348157b96bdf9c17befb4c4a0025eacebb0c6
hanxie <hanxie.wq@alibaba-inc.com>
2023-11-16 21:48:36 +0800

    update: add BUILD_ID

---
+++ src/agentcraft-all/agentcraft-fe/.next/prerender-manifest.js
@@ -0,0 +1,1 @@
+self.__PRERENDER_MANIFEST="{\"version\":4,\"routes\":{},\"dynamicRoutes\":{},\"preview\":{\"previewModeId\":\"90439acdf8b83739c1f29c46bc07451f\",\"previewModeSigningKey\":\"62b0f81fbf870fb447c574395d2e50a14349021af378efda07daa805df55ca44\",\"previewModeEncryptionKey\":\"0ce2b0748a34082f6768625b22bb8dc593ae652c848f15e2278201e5ad7f48b2\"},\"notFoundRoutes\":[]}"
