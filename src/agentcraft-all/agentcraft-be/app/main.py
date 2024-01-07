a815ecee17e3dc0a98108464f21719494905973c
wss <wssgryx@163.com>
2024-01-07 17:31:58 +0800

    fect: python 代码在开发环境可以热更新

--- src/agentcraft-all/agentcraft-be/app/main.py
+++ src/agentcraft-all/agentcraft-be/app/main.py
@@ -11,10 +11,9 @@ if os.environ.get("CREATE_TABLES", False):
 
 if __name__ == "__main__":
     import uvicorn
-    # TODO: reload 需要判断一下环境信息，如果是生产环境则需要将之设置成False
     uvicorn.run("server:app",
                 host="0.0.0.0",
                 port=8000,
                 forwarded_allow_ips="*",
                 workers=int(os.environ.get("WORKERS", 1)),
-                reload=True) # More than 1 worker seems not supported
+                reload=os.environ.get('ENVIRONMENT') == 'development') # More than 1 worker seems not supported
