1a0a6ee9e97d1b0895009508f9ca3319b9c955af
wss <wssgryx@163.com>
2024-01-07 17:24:29 +0800

    fect: python 代码在开发环境可以热更新

--- src/agentcraft-all/agentcraft-be/app/main.py
+++ src/agentcraft-all/agentcraft-be/app/main.py
@@ -1,6 +1,5 @@
 """Main Entry"""
 import os
-import uvicorn
 try:
     from dotenv import load_dotenv
     load_dotenv()  # take environment variables from .env.
@@ -9,8 +8,13 @@ except ModuleNotFoundError:
 from app.db import create_tables
 if os.environ.get("CREATE_TABLES", False):
     create_tables()
-uvicorn.run("server:app",
-            host="0.0.0.0",
-            port=8000,
-            forwarded_allow_ips="*",
-            workers=int(os.environ.get("WORKERS", 1))) # More than 1 worker seems not supported
+
+if __name__ == "__main__":
+    import uvicorn
+    # TODO: reload 需要判断一下环境信息，如果是生产环境则需要将之设置成False
+    uvicorn.run("server:app",
+                host="0.0.0.0",
+                port=8000,
+                forwarded_allow_ips="*",
+                workers=int(os.environ.get("WORKERS", 1)),
+                reload=True) # More than 1 worker seems not supported
